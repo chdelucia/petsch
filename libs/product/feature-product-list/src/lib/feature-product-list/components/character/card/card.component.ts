b751c7652c4604163db0c4838fb0448ee1486bc1
Christian Heredia <cheredianaranjo.it@hbplat.com>
2026-04-09 21:49:43 +0200

    feat(products): added add to cart button

--- libs/product/feature-product-list/src/lib/feature-product-list/components/character/card/card.component.ts
+++ libs/product/feature-product-list/src/lib/feature-product-list/components/character/card/card.component.ts
@@ -3,10 +3,11 @@ import { Component, input, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Product } from '@petsch/api';
 import { CurrentTransitionService } from '../../../current-transition.service';
+import { Button } from '@petsch/ui';
 
 @Component({
   selector: 'lib-product-card',
-  imports: [RouterLink],
+  imports: [RouterLink, Button],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
