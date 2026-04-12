5759a13edd9d3cad9f22d0b7552e2d9ef788acd6
Chris Heredia <c.heredia87@gmail.com>
2026-04-12 09:39:46 +0200

    Update cart-footer.ts

--- libs/shared/ui/src/lib/cart-footer/cart-footer.ts
+++ libs/shared/ui/src/lib/cart-footer/cart-footer.ts
@@ -1,19 +1,16 @@
 import { Component, input, output } from '@angular/core';
 import { Button } from '../button/button';
 import { TranslocoDirective } from '@jsverse/transloco';
-import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'lib-ui-cart-footer',
-  imports: [Button, TranslocoDirective, CurrencyPipe],
+  imports: [Button, TranslocoDirective],
   templateUrl: './cart-footer.html',
   styleUrl: './cart-footer.css',
 })
 export class CartFooter {
   subtotal = input<number>(0);
-  checkoutLabel = input<string>();
   closeLabel = input<string>();
 
-  checkoutClick = output<void>();
   closeDrawer = output<void>();
 }
