2291c78c3ff56834a88f3c368f07c167ce24904c
Chris Heredia <c.heredia87@gmail.com>
2026-04-12 12:19:10 +0200

    Update feature-page.ts

--- libs/product/feature-page/src/lib/feature-page/feature-page.ts
+++ libs/product/feature-page/src/lib/feature-page/feature-page.ts
@@ -1,13 +1,12 @@
 import { Component, effect, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Meta, Title } from '@angular/platform-browser';
 import {
   FeatureProductList,
   ProductsStore,
 } from '@petsch/feature-product-list';
 import { PETLIST_STORE, PETOFDAY_STORE } from '@petsch/api';
 import { FeaturePetOfDay, PetOfTheDayStore } from '@petsch/feature-pet-of-day';
-import { TranslocoDirective, TranslocoService } from '@jsverse/transloco';
+import { TranslocoDirective } from '@jsverse/transloco';
 import { ListHeader } from '@petsch/ui';
 import { FeatureFilters } from '@petsch/feature-filters';
 
@@ -34,10 +33,6 @@ import { FeatureFilters } from '@petsch/feature-filters';
   styleUrl: './feature-page.css',
 })
 export class FeaturePage {
-  private readonly titleService = inject(Title);
-  private readonly metaService = inject(Meta);
-  private readonly translocoService = inject(TranslocoService);
-
   showFilters = signal(true);
   gridView = signal(true);
   showPotdDrawer = signal(false);
@@ -48,23 +43,5 @@ export class FeaturePage {
 
   toggleView(): void {
     this.gridView.update((v) => !v);
-  }
-
-  constructor() {
-    effect(() => {
-      this.updateSeo();
-    });
-
-    this.translocoService.langChanges$.pipe(takeUntilDestroyed()).subscribe(() => {
-      this.updateSeo();
-    });
-  }
-
-  private updateSeo(): void {
-    this.titleService.setTitle(this.translocoService.translate('homeTitle'));
-    this.metaService.updateTag({
-      name: 'description',
-      content: this.translocoService.translate('homeDescription') || '',
-    });
   }
 }
