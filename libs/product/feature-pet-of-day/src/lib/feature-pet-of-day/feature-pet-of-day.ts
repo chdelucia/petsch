62983c36281e1c9db05e594faf184201d921adce
Christian Heredia <cheredianaranjo.it@hbplat.com>
2026-04-12 10:15:49 +0200

    fix: remove css

--- libs/product/feature-pet-of-day/src/lib/feature-pet-of-day/feature-pet-of-day.ts
+++ libs/product/feature-pet-of-day/src/lib/feature-pet-of-day/feature-pet-of-day.ts
@@ -6,7 +6,7 @@ import { PETOFDAY_STORE } from '@petsch/api';
 @Component({
   selector: 'lib-feature-pet-of-day',
   imports: [CartDrawer, CartItem, TranslocoDirective],
-  templateUrl: './feature-pet-of-day.html'
+  templateUrl: './feature-pet-of-day.html',
 })
 export class FeaturePetOfDay {
   protected readonly potdStore = inject(PETOFDAY_STORE);
