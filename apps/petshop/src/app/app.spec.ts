93d29a856ff897801a63ce59f1ed62ba28fa3391
Christian Heredia <cheredianaranjo.it@hbplat.com>
2026-04-12 11:39:27 +0200

    test: add transloco

--- apps/petshop/src/app/app.spec.ts
+++ apps/petshop/src/app/app.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { getTranslocoTestingModule } from '@petsch/shared-utils';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App, getTranslocoTestingModule()],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   });
 
